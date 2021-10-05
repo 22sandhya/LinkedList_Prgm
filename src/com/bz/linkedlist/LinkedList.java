ccd615c3d67e66d3b06436c4caa4bfc755a5441f
Admin <sandhyabagale10@gmail.com>
2021-10-05 23:47:27 +0530

    Added:Add data at start

--- src/com/bz/linkedlist/LinkedList.java
+++ src/com/bz/linkedlist/LinkedList.java
@@ -1,39 +1,65 @@
 package com.bz.linkedlist;
 
-class Node {
-	int data;
-	Node next;
+	class Node {
+		int data;
+		Node next;
 
-	public Node(int data) {
-		this.data = data;
-		this.next = null;
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
 	}
-}
 
-public class LinkedList {
-
-	public static void main(String[] args) {
-
-		System.out.println(" Welcome to LinkList Programs");
-
-		Node firstnode = new Node(56);
-		Node secondnode = new Node(30);
-		Node thirdnode = new Node(70);
+	class operations {
 
 		/*
-		 * created a head variable to store the first node address then we are storing
-		 * the address of next nodes in variable next
+		 * Taking variables to store the head
 		 */
+		Node head;
 
-		Node head = firstnode;
-		firstnode.next = secondnode;
-		secondnode.next = thirdnode;
+		public void pushData(int data) {
 
-		Node temp = head;// storing the head address into temp variable for print the values
-		while (temp != null) {
-			System.out.print(" -> " + temp.data);
-			temp = temp.next;
+			// created the object of the Node class to get the address of the Node and
+			// passing the "data" it will store in constructor
+			Node newnode = new Node(data);
+
+			if (head == null) {
+
+				head = newnode;
+
+			} else {
+				Node temp = head;
+				this.head = newnode;
+				newnode.next = temp;
+			}
 		}
 
+		public void print() {
+			if (head == null)
+				System.out.println("Empty LinkedList");
+
+			else {
+
+				Node temp = head;
+				while (temp != null) {
+					System.out.print(temp.data + " -> ");
+					temp = temp.next;
+				}
+			}
+		}
 	}
+
+	public class LinkedList {
+
+		public static void main(String[] args) {
+
+			System.out.println("* Welcome To LinkList Program *");
+
+			operations link = new operations();
+			link.pushData(70);
+			link.pushData(30);
+			link.pushData(56);
+			link.print();
+		}
+
 }
